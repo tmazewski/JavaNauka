9c0ae32972ba4e9fd3d1e585f511320b4d7dd55a
Student <dfasd@dsfasd.pl>
2024-10-12 12:42:13 +0200

    Examples

---
+++ przyklady/Logic.java
@@ -0,0 +1,13 @@
+package przyklady;
+
+//https://python101.readthedocs.io/pl/latest/podstawy/pythonizmy.html
+
+public class Logic {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(a || b);
+        System.out.println(a && b);
+    }
+}
---
+++ przyklady/Main.java
@@ -0,0 +1,24 @@
+package przyklady;
+
+public  class Main {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    int c = a + b;
+    int sample = 112;
+
+    System.out.println("Hello World "+ sample++);
+    System.out.println("Hello World "+ sample);
+
+    System.out.println("Hello World "+ ++sample);
+    System.out.println("Hello World "+ sample);
+
+    System.out.println("------------------------");
+
+    System.out.println("Hello World "+ sample--);
+    System.out.println("Hello World "+ sample);
+
+    System.out.println("Hello World "+ --sample);
+    System.out.println("Hello World "+ sample);
+  }
+}
