700a9c41af4d443dec0257d304c933006659de3f
Student <dfasd@dsfasd.pl>
2024-10-12 12:45:08 +0200

    Examples

--- przyklady/Logic.java
+++ przyklady/Logic.java
@@ -6,6 +6,7 @@ public class Logic {
     public static void main(String[] args) {
         boolean a = true;
         boolean b = false;
+        System.out.println("hello");
 
         System.out.println(a || b);
         System.out.println(a && b);
